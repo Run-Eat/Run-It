f0b9c6b8b30b182c7ed5ad82e6f87083f01cf7ff
mysubmin <gnasus2884@naver.com>
2024-02-26 01:41:46 +0900

    [Feat] Half screen 구현

--- Run-It/RunningMapView/MapView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MapView.swift
-//  Run-It
-//
-//  Created by t2023-m0039 on 2/22/24.
-//
-
-import Foundation
---
+++ Run-It/RunningMapView/RunningMapView.swift
@@ -0,0 +1,8 @@
+//
+//  RunningMapView.swift
+//  Run-It
+//
+//  Created by t2023-m0039 on 2/25/24.
+//
+
+import Foundation
