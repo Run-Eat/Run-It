b4c4d432651dd633e57fff4d79f2792b460a5139
mysubmin <gnasus2884@naver.com>
2024-02-27 19:36:59 +0900

    [Feat] 시간 컬렉션 추가

---
+++ Run-It/RunningMapView/AmenitiesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AmenitiesCollectionViewCell.swift
+//  Run-It
+//
+//  Created by t2023-m0039 on 2/27/24.
+//
+
+import UIKit
+
+class AmenitiesCollectionViewCell: UICollectionViewCell {
+    
+}
--- Run-It/RunningMapView/DistanceCollectionViewCell.swift
+++ Run-It/RunningMapView/DistanceCollectionViewCell.swift
@@ -2,26 +2,11 @@
 //  DistanceCollectionViewCell.swift
 //  Run-It
 //
-//  Created by t2023-m0039 on 2/26/24.
+//  Created by t2023-m0039 on 2/27/24.
 //
 
 import UIKit
 
 class DistanceCollectionViewCell: UICollectionViewCell {
     
-    var titleLabel: UILabel = {
-        let label = UILabel()
-        return label
-    }()
-    
-    private func addSubviews() {
-        addSubview(titleLabel)
-    }
-
-    private func configure() {
-        titleLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-        backgroundColor = .placeholderText
-    }
 }
