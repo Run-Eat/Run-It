3e93e166a5cce236f37ccd6bd8722a45b0c2964b
futuremouse <kimsoyoonnv@naver.com>
2024-03-04 17:04:26 +0900

    BookmarkViewController-UI

--- Run-It/BookmarkView/BookmarkViewController.swift
+++ Run-It/BookmarkView/BookmarkViewController.swift
@@ -4,28 +4,93 @@
 //
 //  Created by Jason Yang on 2/21/24.
 //
+#if DEBUG
+import SwiftUI
+struct FPreview: UIViewControllerRepresentable {
 
+    // 여기 ViewController를 변경해주세요
+    func makeUIViewController(context: Context) -> UIViewController {
+        BookmarkViewController()
+    }
+
+    func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+    }
+}
+
+struct ViewController_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            FPreview()
+                .edgesIgnoringSafeArea(.all)
+                .previewDisplayName("Preview")
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+        }
+    }
+}
+#endif
 import UIKit
 
-class BookmarkViewController: UIViewController {
+class BookmarkViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var tableView = UITableView()
+    
+    var myFavorite: UILabel = {
+        let label = UILabel()
+        label.text = "즐겨찾기"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .left
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
         view.backgroundColor = .white
-        
-        let label = UILabel()
-        label.text = "bookmarkVC"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+        setupFavoriteUI()
 
+    }
+    // MARK: - UI Setup
+    func setupFavoriteUI() {
+        setupFavoriteListUI()
+    }
+    
+    func setupFavoriteListUI() {
+        view.addSubview(myFavorite)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.register(FavoriteViewCell.self, forCellReuseIdentifier: "FavoriteCell")
+        tableView.backgroundColor = UIColor.white
+        tableView.isScrollEnabled = false
+        
+        myFavorite.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(20)
+        }
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(myFavorite.snp.bottom).offset(15)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(3 * 120)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath) as? FavoriteViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
 }
 
---
+++ Run-It/BookmarkView/FavoriteViewCell.swift
@@ -0,0 +1,95 @@
+//
+//  FavoriteViewCell.swift
+//  Run-It
+//
+//  Created by t2023-m0024 on 3/4/24.
+//
+
+import UIKit
+import SnapKit
+
+class FavoriteViewCell: UITableViewCell {
+    // MARK: - Properties
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.text = "시청gs편의점"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var adressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "서울시 종로구 "
+        label.textAlignment = .right
+        return label
+    }()
+    
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.text = "편의점 "
+        label.textColor = UIColor.secondaryLabel
+        label.textAlignment = .right
+        return label
+    }()
+    
+    // MARK: - Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: "FavoriteCell")
+        setupUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Cell Configuration
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    private func setupUI() {
+        backgroundColor = UIColor.white
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(adressLabel)
+        contentView.addSubview(categoryLabel)
+    }
+    
+    private func setupLayout() {
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.snp.top).offset(15)
+            make.leading.equalTo(contentView.snp.leading).offset(18)
+            make.trailing.equalTo(contentView.snp.trailing).offset(-18)
+            make.height.equalTo(18)
+        }
+        
+        categoryLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(15)
+            make.leading.equalTo(contentView.snp.leading).offset(18)
+//            make.trailing.equalTo(contentView.snp.trailing).offset(-18)
+            make.height.equalTo(18)
+        }
+        adressLabel.snp.makeConstraints { make in
+            make.top.equalTo(categoryLabel.snp.bottom).offset(10)
+            make.leading.equalTo(contentView.snp.leading).offset(18)
+//            make.trailing.equalTo(contentView.snp.trailing).offset(-18)
+            make.height.equalTo(18)
+        }
+    }
+    // MARK: - Layout & Drawing
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.systemGray6
+        backgroundView.layer.cornerRadius = 10
+        backgroundView.layer.masksToBounds = true
+        backgroundView.frame = self.bounds.inset(by: UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0))
+        self.backgroundView = backgroundView
+    }
+}
