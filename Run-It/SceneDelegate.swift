748c0a499c1e57b2f9bcc275c38cf2057a5d4afe
JasonYang <didwnsgus@gmail.com>
2024-03-04 10:38:28 +0900

    [Feat]#23- BackgroundTimer -DispatchSourceTimer를 활용한 타이머 구현 -앱 화면 활성화(백그라운드, 포그라운드) 여부에 따른 타이머 시간 누적 -코어데이터를 활용한 러닝기록 프로퍼티 저장

--- Run-It/SceneDelegate.swift
+++ Run-It/SceneDelegate.swift
@@ -9,6 +9,8 @@ import UIKit
 import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    let runningTimer = RunningTimer()
 
     var window: UIWindow?
 
@@ -46,6 +48,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        runningTimer.state = .foreground
+        runningTimer.timerWillEnterForeground()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -59,6 +63,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+        runningTimer.state = .background
+        runningTimer.timerEnterBackground()
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
